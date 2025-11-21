f12a169cccb05d65a436270d3641b2e648d48bd5
Pollin0tes <pdimatteo@itsmeccanicabruzzo.eu>
2025-11-21 16:31:26 +0100

    Add files via upload

---
+++ puntatori_ALLE_STRUTTURE.cpp
@@ -0,0 +1,11 @@
+//Puntatori e Strutture in C
+//Un puntatore a una struttura è una variabile che memorizza l'indirizzo di una struttura,
+// invece di memorizzare il valore della struttura stessa.
+
+#include <iostream>
+
+int main()
+{
+
+}
+
